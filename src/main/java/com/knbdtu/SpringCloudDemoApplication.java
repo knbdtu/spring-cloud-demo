06db432e025555f46e0abe2da14e1bccadc98a01
knbdtu <knbdtu@hotmail.com>
2023-01-09 23:28:37 +0530

    Making DB Password encrypted

--- src/main/java/com/knbdtu/SpringCloudDemoApplication.java
+++ src/main/java/com/knbdtu/SpringCloudDemoApplication.java
@@ -1,12 +1,14 @@
 package com.knbdtu;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnableEncryptableProperties
 public class SpringCloudDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudDemoApplication.class, args);
